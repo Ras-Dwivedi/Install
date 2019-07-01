1a63d0ce0cb686c48f80684f7c439af04b5adc51
Ras-Dwivedi <rasd.phd@gmail.com>
2019-07-01 12:21:41 +0530

    added -y to few installations

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update 
 
 #installing guake
-sudo apt-get install guake
+sudo apt-get install -y guake
 echo "guake terminal installed"
 sleep 5
 
@@ -30,9 +30,9 @@ echo "SUBLIME TEXT INSTALLED"
 sleep 5
 
 #installing evernote via tusknote
-sudo snap install tusk --classic
-echo "EVERNOT INSTALLED"
-sleep 5
+#sudo snap install tusk --classic
+#echo "EVERNOTE INSTALLED"
+#sleep 5
 
 #installing Qbit torrents
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
@@ -40,43 +40,44 @@ sudo apt update && sudo apt install qbittorrent -y
 echo "QBIT-TORRENT INSTALLED"
 sleep 5
 
-#installing virtual
-#better to installed via binaries in http://www.oracle.com/technetwork/server-storage/virtualbox/downloads/index.html
-sudo apt-get update
-sudo apt-get upgrade
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib"
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install virtualbox-5.2 -y
-echo "VIRTUALBOX INSTALLED"
-sleep 5
+##installing virtual
+##better to installed via binaries in http://www.oracle.com/technetwork/server-storage/virtualbox/downloads/index.html
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib"
+#wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+#wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install virtualbox-5.2 -y
+#echo "VIRTUALBOX INSTALLED"
+#sleep 5
 
 #installing slack
-sudo snap install slack --classic
+sudo snap install -y slack --classic
 echo "SLACK INSTALLED"
 sleep 3
 
 #install flash plugin
-sudo apt-get install flashplugin-installer
+sudo apt-get install -y flashplugin-installer
 echo "flashplugin-installer installed "
 sleep 3
+
 #installing team-viewer
 wget "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
 dpkg -i ~/Downloads/teamviewer_amd64.deb
-apt-get install -f
+sudo apt-get install -f
 echo "TEAM-VIEWER INSTALLED"
 rm ~/Downloads/teamviewer_amd64.deb
 sleep 5
 
 #installing vim
-sudo apt install vim
+sudo apt install -y vim
 echo "VIM INSTALLED SUCCESSFULLY"
 sleep 3
 
 #installing hyperledger
-chmod +x install_hyperledger.sh
-./install_hyperledger.sh
+#chmod +x install_hyperledger.sh
+#./install_hyperledger.sh
 
 wget "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb"
 dpkg -i dropbox_2015.10.28_amd64.deb
@@ -86,10 +87,10 @@ rm ~/Downloads/dropbox_2015.10.28_amd64.deb
 echo "Deleting used files for dropbox"
 sleep 5
 
-#Installing DC++
-sudo apt-get install linuxdcpp
-echo "DC++ INSTALLED"
-sleep 5
+##Installing DC++
+#sudo apt-get install linuxdcpp
+#echo "DC++ INSTALLED"
+#sleep 5
 
 #Installing mailspring
 sudo snap install mailspring
@@ -97,17 +98,17 @@ echo "Installed MailSprin : an email client"
 sleep 5
 
 #Installing java
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
 echo $(java --version)
 echo $(javac --version)
 echo "Installed java "
 sleep 5
 
 #Installing scala
-#sudo apt-get install -y scala
-#echo "Installed scala"
+sudo apt-get install -y scala
+echo "Installed scala"
 
 #Installing vlc
-sudo apt-get install -y vim
+sudo apt-get install -y vlc
 echo "Installed VLC :)"
